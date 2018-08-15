d2f42ccd8b62a5493536564259b562a6ed55e8b3
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2018-08-15 23:26:15 +0530

    Update lottery-money-itr1.c

--- GFG/lottery-money/lottery-money-itr1.c
+++ GFG/lottery-money/lottery-money-itr1.c
@@ -1,3 +1,20 @@
-int main() {
-	return 0;
+#include <stdio.h>
+
+int main ()
+{
+    int target = 15;
+    int max_m = 3;
+    int N = 1;
+    int M = max_m;
+
+    while ( N != target ) {
+        N++;
+        M--;
+        if ( M == 0 ) {
+            max_m = max_m * 2;
+            M = max_m;
+        }
+    }
+    printf("%d\n",M);
+    return 0;
 }
