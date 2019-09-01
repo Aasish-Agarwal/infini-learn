68b61a80e16676ab61687ff4c5e9b1aecd0acb25
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2019-09-01 19:00:09 +0530

    Update el003-v2.c

--- exercises/elementry/el-003/el003-v2.c
+++ exercises/elementry/el-003/el003-v2.c
@@ -1,1 +1,46 @@
 
+#include <stdio.h>
+
+void GreetIfStartingWithVowel(char name[]) 
+{
+	switch ( name[0] ) 
+	{
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+		case 'A':
+		case 'E':
+		case 'I':
+		case 'O':
+		case 'U':
+			printf("Hello %s\n" , name);
+			break;
+	}
+}
+
+void RunTests()
+{
+	// this must print
+	GreetIfStartingWithVowel("amit");
+	GreetIfStartingWithVowel("Amit");
+	GreetIfStartingWithVowel("elina");
+	GreetIfStartingWithVowel("Elina");
+	GreetIfStartingWithVowel("isha");
+	GreetIfStartingWithVowel("Isha");
+	GreetIfStartingWithVowel("omi");
+	GreetIfStartingWithVowel("Omi");
+	GreetIfStartingWithVowel("udai");
+	GreetIfStartingWithVowel("Udai");
+
+	// this must not print
+	GreetIfStartingWithVowel(" ajay");
+	GreetIfStartingWithVowel("");
+	GreetIfStartingWithVowel("Vikas");
+}
+int main()
+{
+	RunTests();
+	return 0;
+}
