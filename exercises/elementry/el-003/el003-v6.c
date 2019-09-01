b29bb73171db798c6c8ac2f0f70ec7abbe3bf8c4
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2019-09-01 19:18:53 +0530

    Create el003-v6.c

---
+++ exercises/elementry/el-003/el003-v6.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int isVowel( char c )
+{
+	int vowel = 0;
+	char vowels[] = "aeiouAEIOU";
+	
+	for ( int i = 0 ; i < sizeof(vowels) / sizeof(char) -1  ; i++ )
+	{
+		if ( vowels[i] == c )
+			vowel = 1;
+	}
+	return vowel;
+}
+
+void GreetIfStartingWithVowel(char name[]) 
+{
+	if ( isVowel(name[0]) ) 
+		printf("Hello %s\n" , name);	
+}
+
+void RunTests()
+{
+	// this must print
+	GreetIfStartingWithVowel("amit");
+	GreetIfStartingWithVowel("Amit");
+	GreetIfStartingWithVowel("elina");
+	GreetIfStartingWithVowel("Elina");
+	GreetIfStartingWithVowel("isha");
+	GreetIfStartingWithVowel("Isha");
+	GreetIfStartingWithVowel("omi");
+	GreetIfStartingWithVowel("Omi");
+	GreetIfStartingWithVowel("udai");
+	GreetIfStartingWithVowel("Udai");
+
+	// this must not print
+	GreetIfStartingWithVowel(" ajay");
+	GreetIfStartingWithVowel("");
+	GreetIfStartingWithVowel("Vikas");
+}
+int main()
+{
+	RunTests();
+	return 0;
+}
